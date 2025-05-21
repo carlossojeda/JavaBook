8b7de09e1888c66b5fb8024b4fada41fcc3ef1aa
Daniel Ojeda <carlosojeda1700@gmail.com>
2025-05-21 01:05:44 -0600

    A hard kata in katas 8 has solved, very hard btw, is the string uppercase?

--- src/CodeWars/Kata8/IsTheStringUpperCase.java
+++ src/CodeWars/Kata8/IsTheStringUpperCase.java
@@ -1,5 +1,23 @@
 package CodeWars.Kata8;
 
-public class IsTheStringUpperCase {
+import java.util.Arrays;
 
+public class IsTheStringUpperCase {
+    public static boolean isUpperCase(String s){
+
+        boolean result = true;
+
+        for(int i = 0; i < s.length(); i++){
+        char letter = s.charAt(i);
+           System.out.println(s.charAt(i));
+           if(Character.isLowerCase(letter)){
+               return false;
+           }
+       }
+       return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUpperCase("HELLO IAMDONALD1"));
+    }
 }
