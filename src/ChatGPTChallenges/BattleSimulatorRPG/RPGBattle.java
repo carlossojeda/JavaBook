61447d699811f86a4d27f228339b1876aaa1f0c0
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-06-05 23:30:43 -0600

    A little progress in the ChatGPT Challenge

--- src/ChatGPTChallenges/BattleSimulatorRPG/RPGBattle.java
+++ src/ChatGPTChallenges/BattleSimulatorRPG/RPGBattle.java
@@ -1,16 +1,33 @@
 package ChatGPTChallenges.BattleSimulatorRPG;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RPGBattle {
-    public static String winnerOfTheBattle(RPGCharacter character1, RPGCharacter character2){
 
-
-
-        return "Hi";
-    }
 
     public static void main(String[] args) {
 
+        //Creación de personajes disponibles
+        RPGCharacter wizard = new RPGCharacter("Wizard", 100, 25, 75, 30);
+        RPGCharacter warrior =   new RPGCharacter("Warrior", 100, 30, 30, 20);
+        RPGCharacter assassin = new RPGCharacter("Assassin", 100, 20, 50, 70);
+
+
+        //Selección de personaje
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escoge un personaje: " +
+                        "\n 1.- wizard " + "Salud: " + wizard.health + " "+  "Poder de ataque: " + wizard.powerAttack + " " + "Defensa: " + wizard.defense + " " + "Velocidad: " + wizard.speed + " " +
+                        "\n 2.- warrior " + "Salud: " + warrior.health + " "+  "Poder de ataque: " + warrior.powerAttack + " " + "Defensa: " + warrior.defense + " " + "Velocidad: " + warrior.speed + " " +
+                    "   \n 3.- assassin " + "Salud: " + assassin.health + " "+  "Poder de ataque: " + assassin.powerAttack + " " + "Defensa: " + assassin.defense + " " + "Velocidad: " + assassin.speed + " ");
+
     }
+
+
+    public static String winnerOfTheBattle(RPGCharacter character1, RPGCharacter character2){
+        return "hi";
+    }
+
+
+
 }
--- src/ChatGPTChallenges/BattleSimulatorRPG/RPGCharacter.java
+++ src/ChatGPTChallenges/BattleSimulatorRPG/RPGCharacter.java
@@ -4,12 +4,15 @@ public class RPGCharacter {
     public String name;
     public int health, powerAttack, defense, speed;
 
-    public  RPGCharacter (String name, int health, int powerAttack, int defense, int speed){
+    public RPGCharacter (String name, int health, int powerAttack, int defense, int speed){
         this.name = name;
         this.health = health;
         this.powerAttack = powerAttack;
         this.defense = defense;
         this.speed = speed;
+
+
+
     }
 
 
