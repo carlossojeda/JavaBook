6de14d9ddef42fc5c7e7e3a4a26dcb91036922df
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-06-16 22:44:57 -0600

    Exercise random numbers done

--- src/Chapter3/GeneratingRandomNumbersWithConditions.java
+++ src/Chapter3/GeneratingRandomNumbersWithConditions.java
@@ -5,6 +5,14 @@ public class GeneratingRandomNumbersWithConditions {
         int numBet0y20 = (int) (Math.random() * 20);
         System.out.println(numBet0y20);
 
-        int numBeth10y20 = (int) (10 * (Math.random() * 20));
+        int numBeth10y20 = (int) (10 + (Math.random() * 10));
+        System.out.println(numBeth10y20);
+
+        int numBeth10y50 = (int) (10 + Math.random() * 41);
+        System.out.println(numBeth10y50);
+
+
+        int zeroOrOne = (int) (Math.random() * 2);
+        System.out.println(zeroOrOne);
     }
 }
