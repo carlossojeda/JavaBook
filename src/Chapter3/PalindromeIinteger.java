f9231f33b50364cfc228662345bec811f5f1a236
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-08-15 21:39:29 -0600

    This exercise was so hard and I solve it in a very hard way

---
+++ src/Chapter3/PalindromeIinteger.java
@@ -0,0 +1,29 @@
+package Chapter3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PalindromeIinteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a three digit integer");
+        String number = sc.nextLine();
+        String numberSPlit = Arrays.toString(number.split(" "));
+        System.out.println(numberSPlit);
+
+        if (numberSPlit.charAt(3) == numberSPlit.charAt(1)) {
+            System.out.println(number + " is a palindrome");
+        } if(numberSPlit.charAt(1) == '-'){
+            if((numberSPlit.charAt(4) == numberSPlit.charAt(2))){
+                System.out.println(number + " is a palindrome");
+            } else System.out.println(number + " is not a palindrome");
+        }
+        else{
+            System.out.println(number + " is not a palindrome");
+        }
+
+
+
+    }
+}
