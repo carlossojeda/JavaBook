95a12ee818dd7d09e48840073969dcb805d0a5e7
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-09-28 14:20:59 -0600

    Just one more exercise

--- src/Chapter3/CompareCosts.java
+++ src/Chapter3/CompareCosts.java
@@ -7,12 +7,26 @@ public class CompareCosts {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the weight and price for package 1: ");
-        double weigh1 = sc.nextDouble(),
+        double weight1 = sc.nextDouble(),
                 price1 = sc.nextDouble();
 
         System.out.println("Enter the weight and price for package 2: ");
-        double weigh2 = sc.nextDouble(),
+        double weight2 = sc.nextDouble(),
                 price2 = sc.nextDouble();
+
+
+
+        double betterPrice = price1 - price2;
+        double betterWeight = weight1 - weight2;
+
+
+        if(betterPrice > price2 && betterWeight > weight2){
+            System.out.println("Package 1 has better price");
+        } else if (betterPrice == price2 && betterWeight == weight2) {
+            System.out.println("Both packages has same price");
+        } else {
+            System.out.println("Package 2 has better price");
+        }
 
 
     }
