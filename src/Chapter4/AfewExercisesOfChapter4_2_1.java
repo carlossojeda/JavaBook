9d5708bdafdfa199d40dc4f8c99ebdf537761b77
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-10-10 14:40:30 -0600

    This examples are about how to use some resources from the Math Object

---
+++ src/Chapter4/AfewExercisesOfChapter4_2_1.java
@@ -0,0 +1,32 @@
+package Chapter4;
+
+public class AfewExercisesOfChapter4_2_1 {
+    public static void main(String[] args) {
+        double resultInRadians = Math.toRadians(47);
+        System.out.println("resultInRadians = " + resultInRadians);
+
+        double resultInDegrees = Math.toDegrees((Math.PI / 7));
+        System.out.println("resultInDegrees = " + resultInDegrees);
+
+        int randomNumber1 =  34 + (int) (Math.random() * 22);
+        System.out.println("randomNumber1 = " + randomNumber1);
+
+        int randomNumber2 = (int) (Math.random() * 1000);
+        System.out.println("randomNumber2 = " + randomNumber2);
+
+        double randomNumber3 = (5.5 + (Math.random() * 50));
+        System.out.println("randomNumber3 = " + randomNumber3);
+
+
+        System.out.println("Math.log(Math.exp(5.5)) = " + Math.log(Math.exp(5.5)));
+        System.out.println("Math.exp(Math.log(5.5)) = " + Math.exp(Math.log(5.5)));
+
+        System.out.println("Math.asin(Math.sin(Math.PI / 6)) = " + Math.asin(Math.sin(Math.PI / 6)));
+        System.out.println("Math.sin(Math.asin(Math.PI / 6)) = " + Math.sin(Math.asin(Math.PI / 6)));
+
+        System.out.println("\"\\u6B22\\u8FCE.\"  = " + "\u6B22\u8FCE." );
+        
+
+        
+    }
+}
