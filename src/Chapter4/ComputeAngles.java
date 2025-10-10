1f965c4ff39d8036e833e26a310aa0af5ff23df4
Carlos Ojeda <carlosojeda1700@gmail.com>
2025-10-09 18:46:47 -0600

    An exercise showing Math Methods

--- src/Chapter4/ComputeAngles.java
+++ src/Chapter4/ComputeAngles.java
@@ -6,7 +6,8 @@ public class ComputeAngles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter three points: ");
+        System.out.println("Enter the coordinates of three points separated " + "by spaces like x1 y1 x2 y2 x3 y3: ");
+
         double x1 = sc.nextDouble(),
                 y1 = sc.nextDouble(),
                 x2 = sc.nextDouble(),
@@ -14,15 +15,15 @@ public class ComputeAngles {
                 x3 = sc.nextDouble(),
                 y3 = sc.nextDouble();
 
-        double a = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
-        double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 -y3));
-        double c = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        double a = Math.sqrt(Math.pow((x2 - x3), 2) + Math.pow((y2 - y3), 2));
+        double b = Math.sqrt(Math.pow((x1 - x3), 2) + Math.pow((y1 - y3), 2));
+        double c = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 
         double A = Math.toDegrees(Math.acos((a * a - b * b - c * c) / (-2 * b * c)));
         double B = Math.toDegrees(Math.acos((b * b - a * a - c * c) / (-2 * a * c)));
         double C = Math.toDegrees(Math.acos((c * c - b * b - a * a) / (-2 * a * b)));
 
-        System.out.println("THe three angles are: " + Math.round(A * 100) / 100.0 + " " + Math.round(B * 100) / (100.0) + " " + Math.round(C * 100 / 100.0));
+        System.out.println("The three angles are: " + Math.round(A * 100) / 100.0 + " " + Math.round(B * 100) / (100.0) + " " + Math.round(C * 100 )/ 100.0);
 
     }
 }
---
+++ src/Chapter4/TestingMathMethods.java
@@ -0,0 +1,23 @@
+package Chapter4;
+
+public class TestingMathMethods {
+    public static void main(String[] args) {
+        System.out.println("Math.sqrt(4) = " + Math.sqrt(4));
+        System.out.println("Math.sin(2 * Math.PI) = " + Math.sin(2 * Math.PI));
+        System.out.println("Math.cos(2 * Math.PI) = " + Math.cos(2 * Math.PI));
+        System.out.println("Math.pow(2, 2) = " + Math.pow(2, 2));
+        System.out.println("Math.log(Math.E) = " + Math.log(Math.E));
+        System.out.println("Math.exp(1) = " + Math.exp(1));
+        System.out.println("Math.max(2, Math.min(3, 4)) = " + Math.max(2, Math.min(3, 4)));
+        System.out.println("Math.rint(-2.5) = " + Math.rint(-2.5));
+        System.out.println("Math.ceil(-2.5) = " + Math.ceil(-2.5));
+        System.out.println("Math.floor(-2.5) = " + Math.floor(-2.5));
+        System.out.println("Math.round(-2.5f) = " + Math.round(-2.5f));
+        System.out.println("Math.round(2.5) = " + Math.round(2.5));
+        System.out.println("Math.rint(2.5) = " + Math.rint(2.5));
+        System.out.println("Math.ceil(2.5) = " + Math.ceil(2.5));
+        System.out.println("Math.floor(2.5) = " + Math.floor(2.5));
+        System.out.println("Math.floor(2.5f) = " + Math.floor(2.5f));
+        System.out.println("Math.round(Math.abs(-2.5)) = " + Math.round(Math.abs(-2.5)));
+    }
+}
